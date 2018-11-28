2ea1ed688300f8714281e99d0589cce96e136890
jared <jaredpattison@users.noreply.github.com>
2018-11-28 15:26:50 -0800

    2 and 3 giddyup

--- challenges10/challenges/challenges-10.test.js
+++ challenges10/challenges/challenges-10.test.js
@@ -30,6 +30,13 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 
 const totalSum = (input) => {
   // Solution code here...
+  let sum = 0;
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      sum = sum + input[i][j];
+    }
+  }
+  return sum;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -46,6 +53,26 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 
 const divisibleByFiveTwoToThePower = (input) => {
   // Solution code here...
+  const final = [];
+  const allFives = [];
+  for (let i = 0; i < input.length; i++) {
+    let fives = [];
+    for (let j = 0; j < input[i].length; j++) {
+      if( input[i][j] % 5 === 0 && typeof(input[i][j]) === 'number') {
+        fives.push(input[i][j]);
+      }
+    }
+    allFives.push(fives);
+  }
+  allFives.forEach(arr => {
+    let temp = [];
+    arr.forEach(num => {
+      let newNum = Math.pow(2, num);
+      temp.push(newNum);
+    })
+    final.push(temp);
+  })
+  return final;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -135,7 +162,7 @@ Run your tests from the console: jest challenges-10.test.js
 
 ------------------------------------------------------------------------------------------------ */
 
-describe('Testing challenge 1', () => {
+xdescribe('Testing challenge 1', () => {
   test('It should return the number of times the input is in the nested arrays', () => {
     expect(count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(4);
     expect(count(3, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(2);
@@ -147,7 +174,7 @@ describe('Testing challenge 1', () => {
   })
 });
 
-describe('Testing challenge 2', () => {
+xdescribe('Testing challenge 2', () => {
   test('It should add all the numbers in the arrays', () => {
     const nums = [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]];
 
@@ -155,7 +182,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return numbers divisible by five, then raise two to the power of the resulting numbers', () => {
     expect(divisibleByFiveTwoToThePower([[10, 20, 5, 4], [5, 6, 7, 9], [1, 10, 3]])).toStrictEqual([[1024, 1048576, 32], [32], [1024]]);
   });
