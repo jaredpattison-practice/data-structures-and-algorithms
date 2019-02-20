c497e367ac33a32e6ec3c9807184e70cf2ead093
jared <jaredpattison@users.noreply.github.com>
2019-02-19 18:30:08 -0800

    quick sort tests pass

---
+++ code-challenges/quickSort/README.md
@@ -0,0 +1,12 @@
+# Quicck sort
+
+# Jared and Heather
+
+## Challenge
+Write a function that accepts an array of integers, and returns an array sorted by a recursive quicksort algorithm.
+
+## Approach & Efficiency
+Recursivly split the array until each array is a length  of 1, then recursivly combinte the arrays, comparing the first index of each array
+
+## Solution
+![](./assets/quick-sort.jpg)
---
+++ code-challenges/quickSort/__tests__/quick-sort.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const mergeSort = require('../quick-sort.js');
+
+describe('mergeSort', () => {
+
+  it ('correctly sorts an array', () => {
+    let unsortedArr = [3, 5, 2];
+    let sortedArr = [2, 3, 5];
+    expect(mergeSort(unsortedArr)).toEqual(sortedArr);
+  });
+
+  it ('handles array with length 1', () => {
+    let unsortedArr = [3];
+    let sortedArr = [3];
+    expect(mergeSort(unsortedArr)).toEqual(sortedArr);
+  });
+
+  it ('handles empty array', () => {
+    let unsortedArr = [];
+    let sortedArr = [];
+    expect(mergeSort(unsortedArr)).toEqual(sortedArr);
+  });
+
+  it ('correctly sorts letters', () => {
+    let unsortedArr = ['a', 'c', 'b'];
+    let sortedArr = ['a', 'b', 'c'];
+    expect(mergeSort(unsortedArr)).toEqual(sortedArr);
+  });
+});
